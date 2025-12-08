66312e1c47d1e646132c6d63b746456ba3b291a6
yunko006 <peyrachethomas@gmail.com>
2025-12-08 13:50:45 +0100

    Initial commit

---
+++ src/server/better-auth/server.ts
@@ -0,0 +1,7 @@
+import { headers } from "next/headers";
+import { cache } from "react";
+import { auth } from ".";
+
+export const getSession = cache(async () =>
+	auth.api.getSession({ headers: await headers() }),
+);
